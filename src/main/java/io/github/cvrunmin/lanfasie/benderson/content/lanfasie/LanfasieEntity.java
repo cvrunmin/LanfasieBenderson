8e1eb2bf61726914b8dc32cd20184f92ae3826df
cvrunmin <cvrunmin@outlook.com>
2026-06-26 22:28:56 +0800

    let lanfasie play songs

--- src/main/java/io/github/cvrunmin/lanfasie/benderson/content/lanfasie/LanfasieEntity.java
+++ src/main/java/io/github/cvrunmin/lanfasie/benderson/content/lanfasie/LanfasieEntity.java
@@ -8,9 +8,12 @@ import com.geckolib.animation.RawAnimation;
 import com.geckolib.animation.object.PlayState;
 import com.geckolib.constant.DefaultAnimations;
 import com.geckolib.util.GeckoLibUtil;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.Mob;
@@ -18,11 +21,37 @@ import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.NoteBlock;
 import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
 
+import java.util.List;
+
 public class LanfasieEntity extends PathfinderMob implements GeoEntity {
     private static final EntityDataAccessor<String> ANIMATE_STATE = SynchedEntityData.defineId(LanfasieEntity.class, EntityDataSerializers.STRING);
+
+    record SongNotes(int[] notes, int[] noteDeltaTick, int duration){
+        public int actualNoteLength(){
+            return Math.min(notes.length, noteDeltaTick.length);
+        }
+    }
+
+    private static final List<SongNotes> SONG_TRACKS = List.of(
+            new SongNotes(new int[]{7, 10, 15, 17, 14, 10, 7, 10, 15, 14, 10, 7},
+                          new int[]{0, 5, 10, 30, 35, 40, 60, 65, 70, 90, 95, 100},
+                          120),
+            new SongNotes(new int[]{13, 13, 16, 15, 13, 13, 16, 15, 13, 13, 16, 15, 13, 13, 16, 15},
+                          new int[]{0, 5, 10, 13, 20, 25, 30, 33, 40, 45, 50, 53, 60, 65, 70, 73},
+                          80),
+            new SongNotes(new int[]{13, 16, 15, 13, 11,  3,  9, 16, 15, 13, 11,  3,  6, 16, 15, 13, 11,  3,  4,  16,  15,  13,  11,  13},
+                          new int[]{ 0,  6, 12, 16, 22, 28, 32, 38, 44, 48, 54, 60, 64, 70, 76, 80, 86, 92, 96, 102, 108, 112, 118, 124},
+                          128)
+            );
+
+    private int playingSongIndex = -1;
+    private int songProgressTick = 0;
+    private int songProgressIndex = -1;
+
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
     public LanfasieEntity(EntityType<? extends PathfinderMob> type, Level level) {
         super(type, level);
@@ -73,6 +102,54 @@ public class LanfasieEntity extends PathfinderMob implements GeoEntity {
 
     public void setAnimateState(String state){
         entityData.set(ANIMATE_STATE, state);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if(level().isClientSide()){
+            if(getAnimateState().equals("play_lute")){
+                if(playingSongIndex == -1){
+                    if (tickCount % 20 == 0 && level().getRandom().nextFloat() < 0.25) {
+                        playingSongIndex = level().getRandom().nextInt(SONG_TRACKS.size());
+                        songProgressTick = 0;
+                        songProgressIndex = 0;
+                        var songTrack = SONG_TRACKS.get(playingSongIndex);
+                        int note = songTrack.notes()[songProgressIndex];
+                        playNote(note);
+                    }
+                }else{
+                    songProgressTick++;
+                    var songTrack = SONG_TRACKS.get(playingSongIndex);
+                    if(songProgressTick > songTrack.duration){
+                        playingSongIndex = -1;
+                        songProgressTick = 0;
+                        songProgressIndex = -1;
+                    }else{
+                        for (int i = songProgressIndex + 1; i < songTrack.actualNoteLength(); i++) {
+                            if(songTrack.noteDeltaTick[i] == songProgressTick){
+                                songProgressIndex = i;
+                                playNote(songTrack.notes[i]);
+                            }else if(songTrack.noteDeltaTick[i] > songProgressTick){
+                                break;
+                            }
+                        }
+                    }
+                }
+            }else{
+                playingSongIndex = -1;
+                songProgressTick = 0;
+                songProgressIndex = -1;
+            }
+        }
+    }
+
+    private void playNote(int note) {
+        if(level().isClientSide()){
+            level().playLocalSound(this, SoundEvents.NOTE_BLOCK_GUITAR.value(), SoundSource.NEUTRAL, 3.0f, NoteBlock.getPitchFromNote(note));
+            var particlePos = position().add(getLookAngle().horizontal().scale(0.5)).add(0, getY(0.5) - getY(), 0);
+            level().addParticle(ParticleTypes.NOTE, particlePos.x, particlePos.y, particlePos.z, note / 24.0, 0.0, 0.0);
+        }
     }
 
     @Override
