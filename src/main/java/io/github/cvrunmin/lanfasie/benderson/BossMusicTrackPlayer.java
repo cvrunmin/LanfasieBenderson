ad2f93f4e4be6dcc74f1f24bfc3b6d5eade62581
cvrunmin <cvrunmin@outlook.com>
2026-07-14 21:40:18 +0800

    update music

--- src/main/java/io/github/cvrunmin/lanfasie/benderson/BossMusicTrackPlayer.java
+++ src/main/java/io/github/cvrunmin/lanfasie/benderson/BossMusicTrackPlayer.java
@@ -1,6 +1,8 @@
 package io.github.cvrunmin.lanfasie.benderson;
 
 import com.mojang.blaze3d.audio.Channel;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.cvrunmin.lanfasie.benderson.foundation.ISoundPositionVisitor;
 import io.github.cvrunmin.lanfasie.benderson.mixin.ChannelHandleAccessor;
 import io.github.cvrunmin.lanfasie.benderson.mixin.SoundEngineAccessor;
@@ -9,28 +11,46 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.sounds.ChannelAccess;
 import net.minecraft.client.sounds.SoundEngine;
 import net.minecraft.core.Holder;
+import net.minecraft.server.packs.metadata.MetadataSectionType;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Mth;
 
+import java.io.IOException;
+import java.util.Optional;
+
 public class BossMusicTrackPlayer {
+    public record MusicTransitionMetadataSection(float crossFadeStartTime, float crossFadeDuration, float crossFadeTargetTime){
+        public static final Codec<MusicTransitionMetadataSection> CODEC = RecordCodecBuilder.create(instance ->
+                instance.group(Codec.FLOAT.fieldOf("crossFadeStartTime").forGetter(MusicTransitionMetadataSection::crossFadeStartTime),
+                        Codec.FLOAT.fieldOf("crossFadeDuration").forGetter(MusicTransitionMetadataSection::crossFadeDuration),
+                        Codec.FLOAT.optionalFieldOf("crossFadeTargetTime", 0f).forGetter(MusicTransitionMetadataSection::crossFadeTargetTime)
+                ).apply(instance, MusicTransitionMetadataSection::new));
+
+        public static final MetadataSectionType<MusicTransitionMetadataSection> TYPE = new MetadataSectionType<>("lanfasie_benderson:music_transition", CODEC);
+    }
+
+
     private final Holder<SoundEvent> soundEvent;
-    private final float musicDuration;
-    private final float crossFadeDuration;
-    private final float crossFadeStartTime;
+    private final float predefinedCrossFadeDuration;
+    private final float predefinedCrossFadeStartTime;
+
+    private boolean initialized;
+    private float crossFadeDuration;
+    private float crossFadeStartTime;
+    private float crossFadeTargetTime;
 
     private boolean active = false;
     private boolean markedDeactivate = false;
     private BossMusicSoundInstance[] tracks = new BossMusicSoundInstance[2];
     private int activeTrack = 0;
 
-    public BossMusicTrackPlayer(Holder<SoundEvent> soundEvent, float musicDuration, float crossFadeDuration, float crossFadeStartTime){
-        if(crossFadeStartTime > musicDuration){
-            throw new IllegalArgumentException("cross-fade start %f > music duration %f".formatted(crossFadeStartTime, musicDuration));
+    public BossMusicTrackPlayer(Holder<SoundEvent> soundEvent, float musicDuration, float predefinedCrossFadeDuration, float predefinedCrossFadeStartTime){
+        if(predefinedCrossFadeStartTime > musicDuration){
+            throw new IllegalArgumentException("cross-fade start %f > music duration %f".formatted(predefinedCrossFadeStartTime, musicDuration));
         }
         this.soundEvent = soundEvent;
-        this.musicDuration = musicDuration;
-        this.crossFadeDuration = crossFadeDuration;
-        this.crossFadeStartTime = crossFadeStartTime;
+        this.predefinedCrossFadeDuration = predefinedCrossFadeDuration;
+        this.predefinedCrossFadeStartTime = predefinedCrossFadeStartTime;
     }
 
     public void startPlaying(){
@@ -43,6 +63,26 @@ public class BossMusicTrackPlayer {
             tracks[0] = BossMusicSoundInstance.fromMusic(soundEvent.value());
             tracks[1] = null;
             Minecraft.getInstance().getSoundManager().play(tracks[0]);
+            if(!initialized){
+                var optionalMeta = Optional.ofNullable(tracks[0].getSound()).flatMap(sound -> Minecraft.getInstance().getResourceManager().getResource(sound.getPath())).flatMap(resource -> {
+                    try {
+                        return resource.metadata().getSection(MusicTransitionMetadataSection.TYPE);
+                    } catch (IOException e) {
+                        return Optional.empty();
+                    }
+                });
+                if(optionalMeta.isPresent()){
+                    var meta = optionalMeta.get();
+                    this.crossFadeDuration = meta.crossFadeDuration;
+                    this.crossFadeStartTime = meta.crossFadeStartTime;
+                    this.crossFadeTargetTime = meta.crossFadeTargetTime;
+                }else{
+                    this.crossFadeDuration = predefinedCrossFadeDuration;
+                    this.crossFadeStartTime = predefinedCrossFadeStartTime;
+                    this.crossFadeTargetTime = 0;
+                }
+                initialized = true;
+            }
         }
         markedDeactivate = false;
     }
@@ -103,7 +143,7 @@ public class BossMusicTrackPlayer {
                                 if(playResult == SoundEngine.PlayResult.STARTED || playResult == SoundEngine.PlayResult.STARTED_SILENTLY){
                                     ChannelAccess.ChannelHandle handle1 = soundEngineAccessor.getInstanceToChannel().get(tracks[nextTrack]);
                                     if(handle1 != null){
-                                        handle1.execute(channel -> ((ISoundPositionVisitor) channel).setSoundPositionSec(sec - crossFadeStartTime));
+                                        handle1.execute(channel -> ((ISoundPositionVisitor) channel).setSoundPositionSec(sec - crossFadeStartTime + crossFadeTargetTime));
                                     }
                                 }
                             }
